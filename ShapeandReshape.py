23c64899eaa157270bf82e66063a9c71b7996251
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-03-02 23:38:09 +0530

    Add files via upload

---
+++ ShapeandReshape.py
@@ -0,0 +1,10 @@
+"""
+Title     : Shape and Reshape
+Subdomain : Numpy
+Domain    : Python
+"""
+import numpy
+
+ar = list(map(int, input().split()))
+np_ar = numpy.array(ar)
+print(numpy.reshape(np_ar, (3, 3)))
